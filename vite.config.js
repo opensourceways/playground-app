e0e00cb1cf018ad000eaab12a40ffbbe295d17ab
devin <devincarl@aliyun.com>
2021-11-29 16:56:05 +0800

    修改base

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import ViteSvgIconsPlugin from "vite-plugin-svg-icons";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   resolve: {
     alias: {
       "@/": `${path.resolve(__dirname, "./src")}/`,
