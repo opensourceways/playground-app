cd8af420eceed392efec4de427c8ba9ea739c8b6
Hu Sheng <tommylikehu@gmail.com>
2021-12-02 19:09:07 +0800

    Update courses.js

--- src/configs/courses.js
+++ src/configs/courses.js
@@ -1,8 +1,8 @@
 export default {
   experience: {
     resource: {
-      id: "1",
-      template: "openeuler-20.03-lts-sp1/container/x86.tmpl",
+      id: "2",
+      template: "openeuler-20.03-lts-sp1/lxd/x86.tmpl",
       // template: "openeuler-20.03-lts-sp1/lxd/x86.tmpl",
       email: "contact@openeuler.io",
     },
