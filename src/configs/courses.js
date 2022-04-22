a282a6ec47421ce111ce3d8fd8e6e0a9ab23410e
Bang <15622356989@163.com>
2022-04-22 15:53:12 +0800

    fix: 修改配置

--- src/configs/courses.js
+++ src/configs/courses.js
@@ -4,7 +4,7 @@ export default {
     resource: {
       // courseId: "2",
       // chapterId: "create-committing-changes",
-      backend: "openEuler-20.03-lts-sp1",
+      backend: "openEuler-22.03-LTS",
       email: "contact@openGauss.org",
       timeout: 30, // 创建资源的超时时间（秒）
       query_interval: 3, // 创建资源后，轮询资源是否ready的间隔时间（秒）
