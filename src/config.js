4f2232f4e4e876652f330c17614b7efee781410e
Bang <15622356989@163.com>
2022-04-25 14:19:33 +0800

    fix: 正式环境修改

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ export const LOGIN_REDIRECT_URI_TEST =
   "https://test.playground.osinfra.cn/login.html";
 // 请求前缀
 export const REQUEST_PREFIX =
-  "https://api.test.osinfra.cn/metadata/v1/metadata/opengauss/moocstudio/courses";
+  "https://api.osinfra.cn/metadata/v1/metadata/opengauss/moocstudio/courses";
 // 资源前缀
 export const ASSETS_PREFIX =
-  "https://api.test.osinfra.cn/metadata/v1/metadata/opengauss/moocstudio/courses";
+  "https://api.osinfra.cn/metadata/v1/metadata/opengauss/moocstudio/courses";
